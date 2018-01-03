944fb65d3210c7b86eb402f75a7f9fd6f571ab7b
zhangyangyang <zhangyangyang@kaike.la>
2018-01-03 21:28:55 +0800

    init

---
+++ lua/core/init.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: yangyang.zhang
+-- Date: 2018/1/3
+-- Time: 16:11
+-- To change this template use File | Settings | File Templates.
+--
+
+local constants = require "constants"
