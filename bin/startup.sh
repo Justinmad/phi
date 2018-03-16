cb3ce8314a859de70c4068f5799a85709420afe7
zhangyangyang <zhangyangyang@kaike.la>
2018-03-16 15:05:15 +0800

    fix startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-mkdir `pwd`/../conf/logs
-nginx -c main.conf -p `pwd`/../conf
+PREFIX = `pwd`/../conf
+ps -ef|grep "nginx -c nginx-test.conf -p /home/tengine/conf"|grep -v grep|awk '{print $2}'|xargs kill
+sleep 5
+mkdir -p ${PREFIX}/logs
+nginx -c main.conf -p ${PREFIX}
