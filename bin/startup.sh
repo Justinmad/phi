fa34215b0846275914cdf09a3193694d845d12ec
zhangyangyang <zhangyangyang@kaike.la>
2018-03-16 15:20:52 +0800

    fix startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 PREFIX=`pwd`/../conf
-ps -ef|grep "nginx -c main.conf -p ${PREFIX}"|grep -v grep|awk '{print $2}'|xargs kill
-sleep 5
-echo "wait 5 sec to kill nginx master process"
+ngx_master_pid=`ps -ef|grep "nginx -c main.conf -p ${PREFIX}"|grep -v grep|awk '{print $2}'`
+if [ -n ${ngx_master_pid} ]; then
+    kill ${ngx_master_pid}
+    echo "wait 5 sec to kill nginx master process"
+    sleep 5
+fi
 mkdir -p ${PREFIX}/logs
 nginx -c main.conf -p ${PREFIX}
